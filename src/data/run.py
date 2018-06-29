ddd542a2e0f398c8ea37d686c27ad345d6442e10
Dionne Ong <dionne_ong@dlsu.edu.ph>
2018-06-29 22:10:35 +0800

    Added threshold  and file reading flexibility

--- src/data/run.py
+++ src/data/run.py
@@ -1,13 +1,15 @@
 import re
 from src.data.ConceptNetAPI.conceptnet import query
 from src.data.filtration.concept_filtration import filter_concept
-from src.data.filtration.concept_filter_config import INCLUDED_RELATIONS
-from src.data.filtration.concept_filter_config import EXCLUDE_ARTICLE
+from src.data.filtration.concept_filter_config import INCLUDED_RELATIONS, EXCLUDE_ARTICLE
 from src.data.db.concepts.DBO_Concept import DBO_Concept
 from src.data.db.concepts.Concept import Concept
 
+recorded_words = []
+thresh = 2.0
 
 def record_capabilities(filename):
+    inner_count = 0
 
     file = open(filename, "r")
     string = file.read()
@@ -16,44 +18,43 @@ def record_capabilities(filename):
     for concept in word_list:
         new_concept = Concept(-1, "character", "CapableOf", concept)
         DBO_Concept.addConcept(new_concept)
+        inner_count += 1
+    return inner_count
+
 
 def record_data(filename):
-
+    inner_count = 0
     file = open(filename, "r")
     string = file.read()
     word_list = re.sub("[^\w]", " ",  string).split()
+
     for word in word_list:
-
-        if word not in EXCLUDE_ARTICLE and not DBO_Concept.does_existn(word):
-
+        if word not in recorded_words and word not in EXCLUDE_ARTICLE:
             for relation in INCLUDED_RELATIONS:
-                #print(word, relation)
-                concept_array_start = query(word, "start", relation, 1.0)
-                concept_array_end   = query(word, "end", relation, 1.0)
+                concept_array_start = query(word, "start", relation, thresh)
+                concept_array_end   = query(word, "end", relation, thresh)
                 concept_array = concept_array_end + concept_array_start
 
+                listed = concept_array
                 listed = filter_concept(concept_array, relation)
-
-                if len(listed) >= 1:
-                    print("Relation: ",relation, listed)
 
                 for concept in listed:
                     if not DBO_Concept.does_exist_relation(concept[0], concept[1], relation):
                         new_concept = Concept(-1, concept[0], relation, concept[1])
                         DBO_Concept.addConcept(new_concept)
+                        inner_count += 1
 
-            print(word, " done")
-        else:
-            print(word, " exists")
+            recorded_words.append(word)
 
     file.close()
+    return inner_count
 
 
 def record_word_data(word):
     if word not in EXCLUDE_ARTICLE:
         for relation in INCLUDED_RELATIONS:
-            concept_array_start = query(word, "start", relation, 1.0)
-            concept_array_end = query(word, "end", relation, 1.0)
+            concept_array_start = query(word, "start", relation, thresh)
+            concept_array_end = query(word, "end", relation, thresh)
             concept_array = concept_array_end + concept_array_start
 
             listed = filter_concept(concept_array, relation)
@@ -62,15 +63,38 @@ def record_word_data(word):
                 new_concept = Concept(-1, concept[0], relation, concept[1])
                 DBO_Concept.addConcept(new_concept)
 
-        print(word, " done")
 
+def restrict_data(filename):
+    inner_count = 0
+    file = open(filename, "r")
+    string = file.read()
+    word_list = re.sub("[^\w]", " ",  string).split()
+    for word in word_list:
+        if word not in recorded_words:
+            recorded_words.append(word)
 
-# record_data("stories/basic concepts")
-# record_data("stories/basic verbs")
-# record_capabilities("stories/basic verb capability")
+    file.close()
 
-#record_data("stories/princess and the pea")
-#record_data("stories/advising a fool")
-#record_data("stories/a cartload of almonds")
-record_data("stories/steadfast tin soldier")
-#record_data("stories/the tinderbox")
+counted = 0
+
+stories = []
+story_count = 0
+
+for text in stories:
+    recorded_count = record_data("text_to_be_processed/"+text)
+    story_count += recorded_count
+    print(text + " = " + recorded_count)
+
+counted += story_count
+
+verbs = record_capabilities("text_to_be_processed/basic verbs")
+print("VERBS: ",verbs)
+counted += counted
+
+capability = record_capabilities("text_to_be_processed/basic verb capability")
+print("CAPABILITY: ",capability)
+counted += counted
+
+print("FINAL STORIES: ", story_count)
+print("FINAL VERBS: ", verbs)
+print("FINAL CAPABILITIES: ", capability)
